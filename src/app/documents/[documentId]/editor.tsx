58d282c7bdc7130e8aa72c64764c9ec87bd1b7b0
khanshahnwaz <shahnwazkhan2007@gmail.com>
2026-01-13 01:46:41 +0530

    added: ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -24,11 +24,12 @@ import Highlight from "@tiptap/extension-highlight";
 import TextAlign from "@tiptap/extension-text-align";
 // import Link from "@tiptap/extension-link";
 import { useEditorStore } from "@/store/use-editor-store";
-
+import { Ruler } from "./ruler";
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -106,6 +107,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-editor-bg px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
