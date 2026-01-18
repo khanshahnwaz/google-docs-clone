49e67c46dabd979046e355f01395de778b425948
khanshahnwaz <shahnwazkhan2007@gmail.com>
2026-01-18 23:02:03 +0530

    updated: Organization feature

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { SearchInput } from "./search-input";
 import { Suspense } from "react";
-import { UserButton } from "@clerk/nextjs";
-export const Navbar = () => {
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
+ export const Navbar = () => {
   return (
     <nav className="flex items-center justify-between h-full w-full">
       <div className="flex gap-3 items-center shrink-0 pr-6">
@@ -16,7 +16,16 @@ export const Navbar = () => {
         <SearchInput />
       </Suspense>
       <div />
-      <UserButton/>
+      {/* <UserButton/> */}
+      <div className="flex gap-3 items-center pl-6">
+        <OrganizationSwitcher
+          afterCreateOrganizationUrl="/"
+          afterLeaveOrganizationUrl="/"
+          afterSelectOrganizationUrl="/"
+          afterSelectPersonalUrl="/"
+        />
+        <UserButton />
+      </div>
     </nav>
   );
 };
